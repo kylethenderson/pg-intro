bf33d7128ce32a03e6a3b0561d643dc7ce78fba5
Kyle Henderson <kthenderson83@gmail.com>
2019-05-29 09:23:43 -0500

    updated documentation

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express'); //import express
+const bodyParser = require('body-parser'); // import body-parser
 
-const app = express();
-const PORT = 5000;
-const songList = require('./modules/songList');
+const app = express(); // create new express app
+const PORT = 5000; // set PORT variable to 5000
+const songList = require('./modules/songList'); // import array called songList
 
-app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('server/public'));   // use express.static method to load index.html
+app.use(bodyParser.urlencoded({extended:true}));    // use bodyParser to have access to req.body later
 
-
+// get route for /songs - sends back the songList array
 app.get('/songs', (req, res) => {
     res.send(songList);
 })
 
+// set app to listen on PORT and console log that message
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
