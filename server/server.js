3867b0c9f23b360d7a4d2e666f38825b939e089d
Kyle Henderson <kthenderson83@gmail.com>
2019-05-29 09:10:39 -0500

    all necessary files created

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
